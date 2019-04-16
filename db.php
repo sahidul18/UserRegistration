b79b805a7e08bcdb2b3b446d0eec43b68e1e3489
sahidul18 <sislam.sahidul@gmail.com>
2019-04-15 23:16:51 -0500

    db name was corrected

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $connection = mysql_connect('localhost', 'root', '',"userDb");
 if (!$connection){
     die("Database Connection Failed" . mysql_error());
 }
-$select_db = mysql_select_db('cs361db');
+$select_db = mysql_select_db('userDb');
 if (!$select_db){
     die("Database Selection Failed" . mysql_error());
 }
